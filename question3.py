e4d37d6c1a30d80a1def42fe7c6b1d2f8ca22377
KING JACOB <jcjeonpds2@gmail.com>
2026-03-23 13:43:14 -0600

    finish question 3

---
+++ question3.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+data = load_breast_cancer()
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.Series(data.target)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+dt_constrained = DecisionTreeClassifier(criterion="entropy",max_depth=3,random_state=42)
+dt_constrained.fit(X_train, y_train)
+
+y_train_predict = dt_constrained.predict(X_train)
+y_test_predict = dt_constrained.predict(X_test)
+
+train_acc_constrained = dt_constrained.score(X_train, y_train)
+test_acc_constrained = dt_constrained.score(X_test, y_test)
+
+feature_importance = pd.Series(dt_constrained.feature_importances_,index=X.columns)
+top5_important_features = feature_importance.sort_values(ascending=False).head(5)
+
+print("Top 5 Important Features: ")
+print(top5_important_features)
+
+print("Training Accuracy Constrained:", train_acc_constrained)
+print("Test Accuracy Constrained:", test_acc_constrained)
+
+#controlling model complexity like max depth allows us to "simplify" the model to prevent
+#the model from memorizing every noise data, preventing overfitting. The training accuracy might become
+#lower, however it is a more accurate represenation of the models performance.
+#Feature importance shows us which features are high information value, and shows us what
+#contributes to the decision making of the model. This contributes to interpretability because
+#it shows us which variables are more likely to cause cancer.
